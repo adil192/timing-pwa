7d117ddc6c86c1bdaa223cd1fbd1c5db09b849e7
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-06-12 10:47:47 +0100

    timing: add blinking

--- timing.ts
+++ timing.ts
@@ -5,6 +5,36 @@ export let resultDescLabel: HTMLParagraphElement;
 
 export let themeColor: string;
 
+let currentMs: number = 500;
+
+enum State {
+	Blinking,
+	Results
+}
+let state: State = State.Blinking;
+
+function timeout(ms): Promise<void> {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let timingThreadRunning: boolean = false;
+async function timingThread() {
+	if (timingThreadRunning) return;
+	timingThreadRunning = true;
+
+	while (true) {
+		if (state == State.Blinking) {
+			square.style.backgroundColor = themeColor;
+			await timeout(currentMs);
+			square.style.backgroundColor = "transparent";
+			await timeout(1000);
+		} else {
+			square.style.backgroundColor = themeColor;
+			await timeout(1000);
+		}
+	}
+}
+
 window.addEventListener("load", () => {
 	square = document.querySelector("square");
 	resultMsLabel = square.querySelector("h2");
@@ -12,6 +42,8 @@ window.addEventListener("load", () => {
 	themeColor = getComputedStyle(document.documentElement).getPropertyValue('--theme-color');
 
 	start();
+
+	timingThread();
 });
 
 function start() {
