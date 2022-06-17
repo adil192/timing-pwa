854f7d95e9a175af86e05415160616c765631cef
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-06-17 12:47:02 +0100

    timing: switch to OOP

--- timing.ts
+++ timing.ts
@@ -1,18 +1,8 @@
 
-export let square: HTMLElement;
-export let resultMsLabel: HTMLHeadingElement;
-export let resultDescLabel: HTMLParagraphElement;
-
-export let themeColor: string;
-
-const fps: number = 60;
-let currentMs: number = 500;
-
-enum State {
+export enum State {
 	Blinking,
 	Results
 }
-let state: State = State.Blinking;
 
 function timeout(ms: number): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -30,59 +20,79 @@ async function cautiousTimeout(ms: number, earlyBreak: () => boolean) {
 	await timeout(ms - elapsed);
 }
 
-let timingThreadRunning: boolean = false;
-async function timingThread() {
-	if (timingThreadRunning) return;
-	timingThreadRunning = true;
+class _Timing {
+	public square: HTMLElement;
+	public resultMsLabel: HTMLHeadingElement;
+	public resultDescLabel: HTMLParagraphElement;
 
-	// noinspection InfiniteLoopJS
-	while (true) {
-		if (state == State.Blinking) {
-			square.style.backgroundColor = themeColor;
-			await cautiousTimeout(currentMs, () => state != State.Blinking);
-			if (state != State.Blinking) continue;
+	public themeColor: string;
 
-			square.style.backgroundColor = "transparent";
-			await cautiousTimeout(1000, () => state != State.Blinking);
-		} else if (state == State.Results) {
-			square.style.backgroundColor = themeColor;
-			await cautiousTimeout(1000, () => state != State.Results);
+	private readonly fps: number = 60;
+	private currentMs: number = 500;
+
+	private state: State = State.Blinking;
+
+	constructor() {
+		window.addEventListener("load", () => {
+			this.square = document.querySelector("square");
+			this.resultMsLabel = this.square.querySelector("h2");
+			this.resultDescLabel = this.square.querySelector("p");
+			this.themeColor = getComputedStyle(document.documentElement).getPropertyValue('--theme-color');
+
+			document.querySelectorAll("#guessesRow button").forEach((btn, i) => {
+				btn.addEventListener("click", () => {
+					let ms: number = parseInt(btn.getAttribute("data-ms"));
+					this.btnClicked(ms);
+				});
+			});
+
+			this.start();
+			this.timingThread().then();
+		});
+	}
+
+	private timingThreadRunning: boolean = false;
+	async timingThread() {
+		if (this.timingThreadRunning) return;
+		let timingThreadRunning = true;
+
+		// noinspection InfiniteLoopJS
+		while (true) {
+			if (this.state == State.Blinking) {
+				this.square.style.backgroundColor = this.themeColor;
+				await cautiousTimeout(this.currentMs, () => this.state != State.Blinking);
+				if (this.state != State.Blinking) continue;
+
+				this.square.style.backgroundColor = "transparent";
+				await cautiousTimeout(1000, () => this.state != State.Blinking);
+			} else if (this.state == State.Results) {
+				this.square.style.backgroundColor = this.themeColor;
+				await cautiousTimeout(1000, () => this.state != State.Results);
+			}
 		}
 	}
+
+	private start() {
+		this.resultMsLabel.innerText = "";
+		this.resultDescLabel.innerText = "";
+		this.square.style.backgroundColor = "transparent";
+
+		this.currentMs = Math.floor(1000 * Math.round(Math.random() * this.fps) / this.fps);
+	}
+
+	private btnClicked(inputMs: number) {
+		if (this.state != State.Blinking) return;
+		this.state = State.Results;
+
+		let diffMs = Math.abs(this.currentMs - inputMs);
+		let diffFrames = Math.round(diffMs * this.fps / 1000);
+
+		this.resultMsLabel.innerText = this.currentMs + "ms";
+		this.resultDescLabel.innerText = `You were ${diffFrames} frames off with your guess of ${inputMs}ms!`;
+	}
+
 }
 
-window.addEventListener("load", () => {
-	square = document.querySelector("square");
-	resultMsLabel = square.querySelector("h2");
-	resultDescLabel = square.querySelector("p");
-	themeColor = getComputedStyle(document.documentElement).getPropertyValue('--theme-color');
+export let Timing = new _Timing();
 
-	document.querySelectorAll("#guessesRow button").forEach((btn, i) => {
-		btn.addEventListener("click", () => {
-			let ms: number = parseInt(btn.getAttribute("data-ms"));
-			btnClicked(ms);
-		});
-	});
-
-	start();
-	timingThread().then();
-});
-
-function start() {
-	resultMsLabel.innerText = "";
-	resultDescLabel.innerText = "";
-	square.style.backgroundColor = "transparent";
-
-	currentMs = Math.floor(1000 * Math.round(Math.random() * fps) / fps);
-}
-
-function btnClicked(inputMs: number) {
-	if (state != State.Blinking) return;
-	state = State.Results;
-
-	let diffMs = Math.abs(currentMs - inputMs);
-	let diffFrames = Math.round(diffMs * fps / 1000);
-
-	resultMsLabel.innerText = currentMs + "ms";
-	resultDescLabel.innerText = `You were ${diffFrames} frames off with your guess of ${inputMs}ms!`;
-}
+console.log(Timing);
